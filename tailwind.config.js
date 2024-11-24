ba02adc2b323d03052b2b8feeeb65b43cebd8841
kenil <kenilsheliya47@gmail.com>
2024-11-24 23:25:11 +0530

    added more sections for this web

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,16 +30,59 @@ module.exports = {
       padding:{
         "10px":"10px",
         "30px":"30px",
+        "92px":"92px",
+        "120px":"120px",
+        "34px":"34px",
+        "3px":"3px",
+        "1px":"1px",
+        "50px":"50px",
+      },
+      margin:{
+        "30px":"30px",
       },
       colors:{
         "dark_green":"#474747",
-        "full_light":"#F9F9F7"
+        "full_light":"#F9F9F7",
+        "primary":"#2C2F24",
+        "nav":"#DBDFD0",
+        "dark_black_btn":"#182226",
+        "hover_bg":"#AD343E",
+        "secondary":"#414536",
       },
       height:{
-        "18px":"18px"
+        "18px":"18px",
+        "100px":"100px"
       },
       width:{
         "18px":"18px",
+        "100px":"100px"
+      },
+      borderWidth:{
+        "1.5px":"1.5px"
+      },
+      fontSize:{
+        "42px":"42px",
+        "100px":"100px",
+        "55px":"55px"
+      },
+      lineHeight:{
+        "30px":"30px",
+        "96px":"96px",
+        "60px":"60px",
+      },
+      letterSpacing:{
+        "0.4px":"0.4px",
+        "3%": "0.03em",
+      },
+      borderRadius:{
+        "34px":"34px",
+        "118px":"118px"
+      },
+      gap:{
+        "90px":"90px",
+      },
+      inset:{
+        "50px":"50px"
       }
     },
   },
